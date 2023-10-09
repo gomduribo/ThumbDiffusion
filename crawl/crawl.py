3c7a03f68f3cedbb7dab7c042cb88102b6088a1e
SongYOungBo <syb6245@naver.com>
2023-10-09 23:34:00 +0900

    restart crawling

--- crawl/crawl.py
+++ crawl/crawl.py
@@ -1,83 +1,42 @@
-import time
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from googleapiclient.discovery import build
+from requests import get  # to make GET request
 
-# + active=""
-# chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_argument('--headless')
-# chrome_options.add_argument('--no-sandbox')
-# # chrome_options.add_argument("--single-process")
-# chrome_options.add_argument("--disable-dev-shm-usage")
-# -
-
-def get_image_title(url):
-    # 웹 드라이버 초기화
-    # driver_path = "C:\\Users\\Rectworks\\PycharmProjects\\chromedriver\\chromedriver.exe"
-    driver_path = r"/home/youngbo/Desktop/ThumbDiffusion/crawl/chromdriver.exe"
-#     driver = webdriver.Chrome(driver_path)
-#     driver = webdriver.Chrome(ChromeDriverManager().install())
-    driver = webdriver.Chrome(executable_path=r"/home/youngbo/Desktop/ThumbDiffusion/crawl/chromedriver")
-    driver.implicitly_wait(5) # or bigger second
-    # 열고자 하는 채널 -> 동영상 목록으로 된 url 페이지를 엶
-    driver.get(url)
-    image_list = list() # 썸네일을 받을 수 있는 주소 저장용 리스트
-    title_list = list() # 썸네일 제목을 저장하는 리스트
-    view_list = list() # 조회수 리스트
-    periods_list = list()
-    idx = 1
-    while True:
-        try:
-            img_xpath = '/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-grid-renderer/div[1]/ytd-grid-video-renderer['+str(idx)+']/div[1]/ytd-thumbnail/a/yt-img-shadow/img'
-            title_xpath = '/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-grid-renderer/div[1]/ytd-grid-video-renderer['+str(idx)+']/div[1]/div[1]/div[1]/h3/a'
-            viewcnt_xpath = '/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-grid-renderer/div[1]/ytd-grid-video-renderer['+str(idx)+']/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/span[1]'
-            period_xpath = '/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-grid-renderer/div[1]/ytd-grid-video-renderer['+str(idx)+']/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/span[2]'
-            # 이미지가 곧바로 로드 되지 않을 때, 20초간 강제로 기다림
-            img = WebDriverWait(driver, 20).until(EC.presence_of_all_elements_located((By.XPATH, img_xpath)))
-            if img is None:
-                print(idx, 'img is not loaded.')
-
-            # 한 페이지에 약 8개 불러오는 데, 동영상 목록을 추가 불러오기 위해 스크롤 내림
-            if idx % 8 == 0 :
-                driver.execute_script('window.scrollBy(0, 1080);')
-                time.sleep(2)
-                driver.execute_script('window.scrollBy(0, 1080);')
-                time.sleep(2)
-                driver.execute_script('window.scrollBy(0, 1080);')
-                time.sleep(2)
-
-            # 썸네일 주소를 리스트에 저장
-            image = driver.find_element_by_xpath(img_xpath)
-            img_url = image.get_attribute('src')
-            image_list.append(img_url)
-
-            # 타이틀을 리스트에 저장
-            title = driver.find_element_by_xpath(title_xpath)
-            title_list.append(title.text)
-
-            # 조회수를 리스트에 저장
-            view = driver.find_element_by_xpath(viewcnt_xpath)
-            view_list.append(view.text)
-
-            # 시간을 리스트에 저장
-            period = driver.find_element_by_xpath(period_xpath)
-            periods_list.append(period)
-            print(idx, title.text, view.text, period.text, img_url)
-
-            idx += 1
-
-        except Exception as e:
-            print()
-            print(e)
-            break
-    assert len(image_list) == len(title_list)
-    # driver.close()
-    return image_list, view_list, periods_list, title_list
-
-# 자이언트 펭TV
-url1 = 'https://www.youtube.com/channel/UCtckgmUcpzqGnzcs7xEqMzQ/videos'
-image1, view1, period1, title1 = get_image_title(url1)
+def download(url, file_name):
+    with open(file_name, "wb") as file:   # open in binary mode
+        response = get(url)               # get request
+        file.write(response.content)      # write to file
 
 
+
+# Replace with your own API key
+# API_KEY = "AIzaSyALGaWS2fifI8pRHIUA9HLwG_0ukcTryQQ"
+API_KEY = "AIzaSyD3cif5mEMSA6YUZviii-QfjsiOraek_4I"
+
+# Create a YouTube API client
+youtube = build('youtube', 'v3', developerKey=API_KEY)
+
+# Call the API to search for videos
+search_response = youtube.search().list(
+    q="침착맨",
+    type='video',
+    part='=id,snippet',
+    location='37, 127',
+    locationRadius='100km',
+    # 한국 동영상 한정
+    order = 'viewCount',
+    # 조회수 순으로 다운
+    maxResults=5
+).execute()
+count = 0
+# Print the titles of the search results
+for search_result in search_response.get('items', []):
+    count+=1
+    print(search_result['id']['videoId'])
+    # print(search_result['snippet']['thumbnails'])
+    if __name__ == '__main__':
+
+        url = "https://img.youtube.com/vi/"+search_result['id']['videoId']+"/maxresdefault.jpg"
+        # youtube thumbnail url을 가져옴
+        download(url,str(count)+".jpg")
+        # 다운로드 경로 설정 필요
+    
